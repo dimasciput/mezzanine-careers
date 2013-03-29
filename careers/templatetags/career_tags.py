4cfce1b54413576811ad5b99105c4f5a988567c5
Sebastián Magrí <sebasmagri@gmail.com>
2013-03-29 01:37:33 -0430

    Fix missing import in career_tags

--- careers/templatetags/career_tags.py
+++ careers/templatetags/career_tags.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.db.models import Count
 
 from careers.forms import JobPostForm
-from careers.models import JobPost
+from careers.models import JobPost, Career
 from mezzanine import template
 
 
