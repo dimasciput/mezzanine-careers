bfb15a61d912e009267617bfde50c66e039e8f9d
Ryan McDevitt <mcdevitt.ryan@gmail.com>
2016-05-14 12:16:44 -0400

    removing keyword migration

--- careers/migrations/0001_initial.py
+++ careers/migrations/0001_initial.py
@@ -33,9 +33,4 @@ class Migration(migrations.Migration):
             name='site',
             field=models.ForeignKey('sites.Site')
         ),
-        migrations.AddField(
-            model_name='jobpost',
-            name='keywords',
-            field=KeywordsField()
-        )
     ]
