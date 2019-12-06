c825f9e2f4c51aa7e12460378c516d802a6b9b63
Dimas Ciputra <dimas.ciputra@gmail.com>
2019-12-06 18:59:38 +0700

    Update urls.py

--- careers/urls.py
+++ careers/urls.py
@@ -1,29 +1,29 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+from .views import jobpost_list, jobpost_detail, 
 
 # Job Post patterns.
-urlpatterns = patterns("careers.views",
-
+urlpatterns = [
     url("^tag/(?P<tag>.*)/$",
-        "jobpost_list",
+        jobpost_list,
         name="jobpost_list_tag"),
 
     url("^archive/(?P<year>\d{4})/(?P<month>\d{1,2})/$",
-        "jobpost_list",
+        jobpost_list,
         name="jobpost_list_month"),
 
     url("^archive/(?P<year>.*)/$",
-        "jobpost_list",
+        jobpost_list,
         name="jobpost_list_year"),
 
     url("^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>.*)/$",
-        "jobpost_detail",
+        jobpost_detail,
         name="jobpost_detail_date"),
 
     url("^(?P<slug>.*)/$",
-        "jobpost_detail",
+        jobpost_detail,
         name="jobpost_detail"),
 
     url("^$",
-        "jobpost_list",
+        jobpost_list,
         name="jobpost_list"),
-)
+]
